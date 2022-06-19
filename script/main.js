2de0462d5b8535f6c8a4fb68eeb185ce67beca1d
Camilono14 <camilo_no@hotmail.com>
2022-06-19 09:33:38 -0700

    first commit

---
+++ script/main.js
@@ -0,0 +1,40 @@
+// este es un comentario en linea
+
+/* Este es un 
+comentario
+en bloque */
+
+// let name = "Camilo"; // solo se usan comillas para palabras o letras.
+
+// let name = "Camilo";
+
+// const pi = 3.1416
+
+
+// console.log("Hola mundo " + pi);
+
+/* name = "Chuncho";
+
+console.log("Hola mundo " + name); */
+
+
+// Tipos de datos
+// number = enteros y decimales 1 2 3 4 876543
+// boolean = true or false
+// string = cadena de texto "1 2345667"
+// undefined = no definido 
+
+// number
+
+const  y = 1;
+let x;
+x = prompt("Ingrese el valor de X");
+console.log("el numero ingresado es:" + x);
+let sum = parseInt(x) + y;
+console.log("la suma es igual a " + sum);
+let res = parseInt(x) - y;
+console.log("la resta es igual a " + res);
+let mul = parseInt(x) * y;
+console.log("la multiplicacion es igual a " + mul);
+let div = parseInt(x) / y;
+console.log("la division es igual a " + div);
